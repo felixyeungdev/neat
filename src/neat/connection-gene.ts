2b8fda830a9d878fd8fe283348e348d4d19def56
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-02-02 11:35:05 +0000

    add class studs

---
+++ src/neat/connection-gene.ts
@@ -0,0 +1,3 @@
+import { NeatGene } from "./gene";
+
+export class NeatConnectionGene extends NeatGene {}
---
+++ src/neat/node-gene.ts
@@ -0,0 +1,3 @@
+import { NeatGene } from "./gene";
+
+export class NeatNodeGene extends NeatGene {}
