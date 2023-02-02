2474e55c3d2bd3cf2dc0a2b1284838ad54b3401f
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-02-02 23:51:39 +0000

    feat: add margin

--- src/visualiser/visualiseGenome.ts
+++ src/visualiser/visualiseGenome.ts
@@ -2,21 +2,27 @@ import { createCanvas } from "canvas";
 import fs from "fs";
 import { NeatGenome } from "../neat/genome";
 
+const addMargin = (value: number, size: number) => {
+  const margin = 0.1;
+  return value * (1 - margin) + (margin * size) / 2;
+};
+
 export const visualiseGenome = (genome: NeatGenome) => {
   const canvas = createCanvas(500, 500);
   const ctx = canvas.getContext("2d");
   const size = 500;
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, size, size);
-  ctx.globalAlpha = 0.5;
 
   const { nodes, connections } = genome;
 
   for (const node of nodes) {
     const { x, y } = node;
+    const canvasX = addMargin(x * size, size);
+    const canvasY = addMargin(y * size, size);
     ctx.fillStyle = "black";
     ctx.beginPath();
-    ctx.arc(x * size, y * size, 5, 0, Math.PI * 2);
+    ctx.arc(canvasX, canvasY, 5, 0, Math.PI * 2);
     ctx.fill();
   }
 
@@ -25,8 +31,14 @@ export const visualiseGenome = (genome: NeatGenome) => {
     if (!enabled) continue;
     ctx.strokeStyle = weight > 0 ? "red" : "blue";
     ctx.beginPath();
-    ctx.moveTo(fromNode.x * size, fromNode.y * size);
-    ctx.lineTo(toNode.x * size, toNode.y * size);
+    ctx.moveTo(
+      addMargin(fromNode.x * size, size),
+      addMargin(fromNode.y * size, size)
+    );
+    ctx.lineTo(
+      addMargin(toNode.x * size, size),
+      addMargin(toNode.y * size, size)
+    );
     ctx.stroke();
   }
 
